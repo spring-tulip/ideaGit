378bc67f6b29e7facdb64fd5409ab4eaeb90b1aa
spring-tulip <56726805+spring-tulip@users.noreply.github.com>
2022-11-25 19:40:21 +0800

    githubTest

--- src/main/java/Controller/GitTest.java
+++ src/main/java/Controller/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("hello git!");
         System.out.println("hello git!");
         System.out.println("hello git!");
+        system.out.printin("github test");
     }
 }
